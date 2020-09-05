b6cfab7137088222d8762c521d35f7c8a80d3265
Hippie Stereo <hippie.stereo@gmail.com>
2020-09-05 12:02:35 +0100

    Update app module

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home.component';
+import { PhotoList } from './components/photo-list.component';
+import { PhotoService } from './services/photo.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PhotoList
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PhotoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
