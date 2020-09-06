a9c9224fb4290f18b2bd3da25861ce36c949115f
Hippie Stereo <hippie.stereo@gmail.com>
2020-09-07 00:58:13 +0100

    Add user controller

---
+++ back-end/Lagoon-Backend/src/main/java/com/hippiestereo/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.hippiestereo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hippiestereo.model.User;
+import com.hippiestereo.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public User registerUser(@RequestBody User user) {
+		
+		return userService.save(user);
+		
+	}
+	
+}
