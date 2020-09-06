8df92bb3f9eb8dbde2d0c2743f1befebfd06bc17
Hippie Stereo <hippie.stereo@gmail.com>
2020-09-06 01:40:04 +0100

    Create project
    Add spring web

---
+++ back-end/Lagoon-Backend/src/main/java/com/hippiestereo/LagoonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hippiestereo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagoonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagoonBackendApplication.class, args);
+	}
+
+}
