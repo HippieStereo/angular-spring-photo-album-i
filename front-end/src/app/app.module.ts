907e6e8b372f9e6dd0a582167613554e6c505cf0
Hippie Stereo <hippie.stereo@gmail.com>
2020-09-06 01:34:27 +0100

    Add js to proj

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home.component';
 import { PhotoList } from './components/photo-list.component';
+import { SidePanel } from './components/side-panel.component';
 import { PhotoService } from './services/photo.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PhotoList
+    PhotoList,
+    SidePanel
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [PhotoService],
   bootstrap: [AppComponent]
