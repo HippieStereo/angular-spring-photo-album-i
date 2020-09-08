2d8466e0d3d4f577255f99fabdf3fda9cecef292
Hippie Stereo <stereobroo@yahoo.com>
2020-09-08 19:48:39 +0100

    Register

---
+++ front-end/src/app/services/register.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../models/user';
+//import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class RegisterService{
+
+    constructor(private http: HttpClient){}
+
+    sendUser(user: User){
+
+        let url = "http://localhost:8080/user/register";
+
+        let headers1 = new HttpHeaders({'Content-Type': 'application/json'});
+
+        return this.http.post(url, JSON.stringify(user), {headers: headers1});
+
+    }
+
+}
