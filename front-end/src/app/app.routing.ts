3945c599065de98e4773b389a218498937f54fb8
Hippie Stereo <stereobroo@yahoo.com>
2020-09-08 19:48:20 +0100

    Update app.routing.ts

--- front-end/src/app/app.routing.ts
+++ front-end/src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home.component';
+import { Register } from './components/register.component';
 
 const appRoutes: Routes = [
     {
@@ -11,10 +12,14 @@ const appRoutes: Routes = [
     {
         path: 'home',
         component: HomeComponent
+    },
+    {
+        path: 'register',
+        component: Register
     }
 ];
 
-// - 
+// - Angular error - Generic type 'ModuleWithProviders<T>' requires 1 type argument(s)
 declare module "@angular/core" {
     interface ModuleWithProviders<T = any> {
         ngModule: Type<T>;
