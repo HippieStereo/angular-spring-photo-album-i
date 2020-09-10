e78701f142f57115326b35095536310367470f11
Hippie Stereo <stereobroo@yahoo.com>
2020-09-10 15:54:04 +0100

    Add jwt

--- back-end/Lagoon-Backend/src/main/java/com/hippiestereo/LagoonBackendApplication.java
+++ back-end/Lagoon-Backend/src/main/java/com/hippiestereo/LagoonBackendApplication.java
@@ -2,25 +2,38 @@ package com.hippiestereo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.hippiestereo.config.JwtFilter;
 
 @SpringBootApplication
 public class LagoonBackendApplication {
 
+	@Bean
+	public FilterRegistrationBean<JwtFilter> jwtFilter(){
+		
+		final FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<JwtFilter>();
+		
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/rest/*");
+		
+		return registrationBean;
+		
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LagoonBackendApplication.class, args);
 	}
 	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
-			}
-		};
-	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("*").allowedOrigins("http://localhost:9000");
+//			}
+//		};
+//	}
 
 }
