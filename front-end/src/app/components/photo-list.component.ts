036a221abe103ff06fbf733905a0d77b0df08b18
HippieStereo <stereobroo@yahoo.com>
2020-09-14 01:05:46 +0100

    Update photo-list.component.ts

--- front-end/src/app/components/photo-list.component.ts
+++ front-end/src/app/components/photo-list.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {PhotoService} from '../services/photo.service';
 import {User} from '../models/user';
 import {Photo} from '../models/photo';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'photo-list',
@@ -12,12 +13,17 @@ export class PhotoList{
     photos: Photo[];
     selectedPhoto: Photo;
 
-    constructor(private photoService: PhotoService){
+    constructor(private photoService: PhotoService, private router: Router){
         this.photoService.getPhotos().subscribe(
             (data: any[])=>{
                 console.log(data);
                 this.photos = data;
-              } )
+              })
     }
+
+    onSelect(photo:Photo) {
+        this.selectedPhoto = photo;
+        this.router.navigate(['/image-detail', this.selectedPhoto.photoId]);
+      }
 
 }
