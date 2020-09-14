6ccf6c17fd1c938f55903f732ab6c8b5b9b47212
HippieStereo <stereobroo@yahoo.com>
2020-09-14 01:03:34 +0100

    Update app.module.ts

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -8,14 +8,20 @@ import { SidePanel } from './components/side-panel.component';
 import { NavBar } from './components/nav-bar.component';
 import { Register } from './components/register.component';
 import { MyAlbum } from './components/my-album.component';
+import { AddPhoto } from './components/add-photo.component';
+import { ImageDetail } from './components/image-detail.component';
+import { ImageComments } from './components/image-comments.component';
 import { PhotoService } from './services/photo.service';
 import { RegisterService } from './services/register.service'
 import { HttpClientModule } from '@angular/common/http';
 import { routing } from './app.routing';
 import { FormsModule } from '@angular/forms';
-import { Login } from './components/login.component'
-import { LoginService } from './services/login.service'
-import { UserService } from './services/user.service'
+import { Login } from './components/login.component';
+import { LoginService } from './services/login.service';
+import { UserService } from './services/user.service';
+import { AddPhotoService } from './services/add-photo.service';
+import { UploadPhotoService } from './services/upload-photo.service';
+import { CommentService } from './services/comment.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +32,10 @@ import { UserService } from './services/user.service'
     NavBar,
     Register,
     Login,
-    MyAlbum
+    MyAlbum,
+    AddPhoto,
+    ImageDetail,
+    ImageComments
   ],
   imports: [
     BrowserModule,
@@ -38,7 +47,10 @@ import { UserService } from './services/user.service'
     PhotoService,
     RegisterService,
     LoginService,
-    UserService
+    UserService,
+    AddPhotoService,
+    UploadPhotoService,
+    CommentService
   ],
   bootstrap: [AppComponent]
 })
